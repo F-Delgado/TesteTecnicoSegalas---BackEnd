bc8ed5858b57419b9d84277ed38ba66d6947ae8a
Fernando Delgado Bautista da Cunha <fdbautista90@gmail.com>
2023-09-27 12:43:34 -0300

    .

---
+++ BackEnd/target/classes/db/migration/V2__create-database-for-test.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS testesegalas_test;
+
+CREATE DATABASE testesegalas_test
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+CREATE DATABASE testesegalas_test
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
